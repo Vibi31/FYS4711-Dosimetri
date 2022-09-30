03db6bf8174c72e54e926d14e64412a9cf84511b
Vibi31 <vrvichu0@gmail.com>
2022-09-30 18:44:01 +0200

    started on mandatory assignment

---
+++ MonteCarlo_sim.py
@@ -0,0 +1,8 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+
+min, max = 50, 2000   #min and max photon energy in kev
+nv = 3.43e23    #number of electrons in water per cubic centimeter 
+
+def atten_coef():
+    sigma  = #electronic cross section for Compton scattering
